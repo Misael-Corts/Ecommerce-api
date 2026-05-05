08eb16d44c2c76b34bc656edaf71a30cac76e881
Misa <misael.cortes39@gmail.com>
2026-05-05 18:08:09 -0300

    add total products count to getProducts response

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -17,7 +17,11 @@ export const createProduct = async (req: Request, res: Response) => {
 export const getProducts = async (req: Request, res: Response) => {
   try {
     const products = await getProductsService(req.query);
-    res.json(products);
+    let count = 0;
+    for (let i = 0; i < products.length; i++) {
+      count++
+    }
+    res.json({products, total_products: count});
   } catch (error) {
     res.status(500).json({ error: "Error fetching products" });
   }
